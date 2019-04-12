f35fc3cc974be88172afbfe7c4369dde28fd3321
Tristan Dlouhy <tristan.dlouhy@nielsen.com>
2019-04-12 10:53:17 +0200

    Add FontAwesome to the project

--- ngrx-store/src/app/app.module.ts
+++ ngrx-store/src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// App imports
 import {
   AppComponent
 } from './components';
+
+// FontAwesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faCoffee,
+} from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
-  bootstrap: [
-    AppComponent
-  ]
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    // Add FontAwesome icons to library
+    library.add(
+      faCoffee
+    );
+  }
+}
